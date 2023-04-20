055d404ea90e9a8620b65fba66408a7de5081379
Eddy Lujan <eddylujann@gmail.com>
2023-04-20 17:54:13 +0200

    upp20

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,153 @@
 (function($) {
     var	$window = $(window), $body = $('body');
-    var parent_sumary = $('._aalv._akmq._akm0');
-    var children_sumary = $('._aak6._aak9'); 
-    var bigPhoneAdd = $('.xx');
-    var children_brother = $('._aam1._akej._akmr');
+    var $ps = $('._aalv._akmq._akm0'), $cs = $('._aak6._aak9'), $cbs = $('._aam1._akej._akmr');
+
+    const screen = {
+        small   : 0     ,
+        medium  : 400   ,
+        large   : 800
+    };
+
+    resizeHandler();
+
+    var resizeId;
+    $(window).resize(function() {
+        clearTimeout(resizeId);
+        resizeId = setTimeout(resizeHandler, 10); //500
+    });
+
+    
+    /**
+     * Controlador de cambio de tamaño del width de la pantalla.
+     */
+    function resizeHandler() {
+        // Obtener ancho de ventana.
+        const iw = $window.width();
+        // Determinar el tamaño y el ancho con nombre.
+        let size = null;
+        let nw = null;
+        for ( let s in screen ) {
+            if ( iw >= screen[ s ] ) size = s; nw = iw;
+        }
+        console.log("size=> " + size + ' and ' + 'new => ' + nw);
+        // Aplicar la media Query en cada tamaño de la pantalla.
+        mediaQuery(nw);
+    }
+
+    
+    /**
+     * ----- [ Cambios en el componente summary ] ---
+     * Borrar e insertar el elemento children summary.
+     * Despues de borrar el elemento childres, se modifica las 
+     * clases del elemento padre.
+     * @param {*} nw 
+     */
+    var $csb;
+    var $temp1, $temp2, $temp3, $temp4;
+    function mediaQuery(nw) {
+        // ----- [ Cambios en el componente summary ] ---
+        if ( nw < 1161 ) {
+            if( $csb ) {
+                $cs.remove();
+                $ps.removeClass( '_akm0' );
+                $cbs.removeClass( '_akmr' );
+            } else {
+                $csb = $cs.detach();
+                $ps.removeClass( '_akm0' );
+                $cbs.removeClass( '_akmr' );
+            }
+        } else {
+            $ps.append($csb);
+            $ps.addClass( '_akm0' );
+            $cbs.addClass( '_akmr' );
+        }
+
+        // ----- [ Cambios en el componente feed ] ---
+        var par = $('#r293q1');
+        var $ix9f619 = $('#ix9f619'); 
+        var $ixvb8j5 = $('#ixvb8j5');
+        var $ix1cy8zhl = $('#ix1cy8zhl');
+        var $ix2lah0s = $('#ix2lah0s');
+        var $ixl5mz7h = $('#ixl5mz7h');
+
+        var $ixh8yej3 = $('#ixh8yej3 > div');
+        var $ix1cy8zhl = $('#ix1cy8zhl');
+
+        if ( nw < 768 ) {
+            console.log("DEBUF: w: 767...");
+            if ( par.hasClass("x1q0g3np") ) {
+                par.addClass('xdt5ytf').removeClass('x1q0g3np');
+                $ix9f619.addClass('xg7h5cd xh8yej3 x1vjfegm x1ey2m1c x80663w x1jeouym x6w1myc').removeClass('xeq5yr9 x1dr59a3  x13vifvy x1n327nk');
+                $ixvb8j5.addClass('xvbhtw8 xh8yej3').removeClass('xvb8j5 x1vjfegm');
+                $ix1cy8zhl.addClass('xh8yej3 x1q0g3np x1n2onr6 xaw8158 xtuw4uo').removeClass('x1cy8zhl xdt5ytf x1gvbg2u x1y1aw1k xn6708d xx6bls6 x1ye3gou xvbhtw8 x1xgvd2v');
+
+                // Se borra el elemento del logo y el element final del nav.
+                if ( $temp1 || $temp2) {
+                    $ix2lah0s.remove();    
+                    $ixl5mz7h.remove();
+                } else {
+                    $temp1 = $ix2lah0s.detach();
+                    $temp2 = $ixl5mz7h.detach();
+                }
+
+                // Pasar divs ($ixh8yej3 > div) o moverlo a otro div.
+                var dd = $ixh8yej3.detach().appendTo($ix1cy8zhl); 
+                console.log("DEBUG: Reuslt : " + dd);
+                if (dd) {
+                    $ixh8yej3.remove();    
+                } else {
+                    $temp3 = $ixh8yej3.detach();
+                }
+            } 
+        } else {
+            if ( par.hasClass("xdt5ytf") ) {
+                par.addClass('x1q0g3np').removeClass('xdt5ytf');
+                $ix9f619.addClass('xeq5yr9 x1dr59a3 x13vifvy x1n327nk xaw8158 xtuw4uo').removeClass('xg7h5cd xh8yej3 x1vjfegm x1ey2m1c x80663w x1jeouym x6w1myc');
+                $ixvb8j5.addClass('xvb8j5 x1vjfegm').removeClass('xvbhtw8 xh8yej3'); 
+                $ix1cy8zhl.addClass('x1cy8zhl xdt5ytf x1gvbg2u x1y1aw1k xn6708d xx6bls6 x1ye3gou xvbhtw8 x1xgvd2v').removeClass('xh8yej3 x1q0g3np x1n2onr6 xaw8158 xtuw4uo');
+
+                /**
+                 * Se buelve a agregar los elementos del logo y el elemento final del nav.
+                 * Es como la función .append() solo que añade el HTML directamente 
+                 * después de la apertura de la etiqueta del elemento seleccionado.
+                 */
+                $ix1cy8zhl.prepend($temp1);
+                $ix1cy8zhl.append($temp2);
+
+                //$('##ix1cy8zhl').append($temp3);
+
+
+               
+            } 
+        }
+    }
+
+
+
+
+
+
+
+    // observe window resize
+    // window.addEventListener('resize', resizeHandler);
+
+    // // initial call
+    // resizeHandler();
+
+    // // calculate size
+    // function resizeHandler() {
+
+    //     // get window width
+    //     const iw = window.innerWidth;
+        
+    //     // determine named size
+    //     let size = null;
+    //     for (let s in screen) {
+    //         if (iw >= screen[s]) size = s;
+    //     }
+
+    //     console.log(size);
+    // }
 
 
     /**
@@ -21,21 +165,36 @@
      * lentamente, lo cual es muy poco probable: )
      */
     
-    var resizeId;
+    
+    /*var resizeId;
     $(window).resize(function() {
         clearTimeout(resizeId);
+        if ( $window.width() <= 767 ) {
+            console.log("DEBUG: resize entro...");
+            $body.css({
+                'background-color': '#ccc',
+                'border' : '2px solid red',
+            });
+        }
+        
         resizeId = setTimeout(doneResizing, 500);
-    });
+    });*/
 
-    function doneResizing(){
-        //whatever we want to do
-    }
+    /*function doneResizing(){
+        // Lo que queramos hacer
+        console.log("DEBUG: doneResizing...");
+        $body.css("border-color","blue");
+        $body.css({
+            'background-color': '#ccc',
+            'border' : '5px solid green',
+        });
+    }*/
 
 
 
 
     // Play initial animations on page load.
-		$window.on('load', function() {
+		/*$window.on('load', function() {
             var windowWidth = $( this ).width();
             windowWidth < 1160 ? appendSumary() : datachSumary();
 
@@ -51,17 +210,12 @@
                     par.addClass('x1q0g3np').removeClass('xdt5ytf');
                 } 
             }
-		});
+		});*/
     
     // Resize width
-        $window.resize(function(e) {
-
-            //var newParentDiv = 'x9f619 xnz67gz x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x1qjc9v5 x1oa3qoh x1qughib'; // add -----> xdt5ytf
-            //var parentDiv = $('x9f619 xnz67gz x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s x1q0g3np xqjyukv x1qjc9v5 x1oa3qoh x1qughib'); // delete -> x1q0g3np
+        /*$window.resize(function(e) {
             var windowWidth = $( this ).width();
             windowWidth < 1160 ? appendSumary() : datachSumary();
-
-            
         });
 
         function appendSumary() {
@@ -76,14 +230,6 @@
             children_brother.addClass( '_akmr' );
             bigPhoneAdd.detach();
             children_sumary.appendTo(parent_sumary);
-        }
-    
-    
-
-    //console.log("DEBUG: Detect widht 767: ");
-    //console.log(parentDiv);
-    //parentDiv.addClass(newParentDiv);
-    //parentDiv.removeClass(parentDiv);
-    //parentDiv.addClass(newParentDiv).removeClass(parentDiv);
+        }*/
 
 })(jQuery);
